f516b872a413e0ceffaef82ed8569338230fbfdd
terrykker <344163452@qq.com>
2022-08-12 16:18:40 +0800

    增加icon和scss样式

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const { series, src, dest } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+const cssmin = require("gulp-cssmin");
+
+function compile() {
+  return src("./src/components/theme-chalk/*.scss")
+    .pipe(sass.sync())
+    .pipe(autoprefixer())
+    .pipe(cssmin())
+    .pipe(dest("./lib/theme-chalk"));
+}
+
+function copyfont() {
+  return src("./src/components/theme-chalk/fonts/**")
+    .pipe(cssmin())
+    .pipe(dest("./lib/theme-chalk/fonts"));
+}
+
+function moveMainCss() {
+  return src("./lib/theme-chalk/index.css").pipe(dest("./dist/theme-chalk"));
+}
+
+exports.build = series(compile, copyfont, moveMainCss);
